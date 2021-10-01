c69bd4fa7cc15e77e25157391890b4502ccb8eb4
Shubham Parashar <shubhamparashar210@gmail.com>
2021-10-01 23:52:33 +0530

    news search select added

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Select, Typography, Row, Col, Avatar, Card } from "antd";
 import moment from "moment";
 
@@ -11,9 +11,10 @@ const demoImage =
   "https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News";
 
 const News = ({ simplified }) => {
+  const [newsCategory, setNewsCategory] = useState("CryptoCurrency");
   const { data } = useGetCryptosQuery(100);
   const { data: cryptoNews, isFetching } = useGetCryptoNewsQuery({
-    newsCategory: "CryptoCurrency",
+    newsCategory,
     count: simplified ? 6 : 12,
   });
 
@@ -22,6 +23,25 @@ const News = ({ simplified }) => {
   return (
     <>
       <Row gutter={[24, 24]}>
+        {!simplified && (
+          <Col span={24}>
+            <Select
+              showSearch
+              className="select-news"
+              placeholder="Select a Coin"
+              optionFilterProp="children"
+              onChange={(value) => setNewsCategory(value)}
+              filterOption={(input, option) =>
+                option.children.toLowerCase().indexOf(input.toLowerCase() >= 0)
+              }
+            >
+              <Option value="CryptoCurrency">CryptoCurrency</Option>
+              {data?.data?.coins.map((coin) => (
+                <Option value={coin.name}>{coin.name}</Option>
+              ))}
+            </Select>
+          </Col>
+        )}
         {cryptoNews.value.map((news, i) => (
           <Col xs={24} sm={12} lg={8} key={i}>
             <Card hoverable className="news-card">
