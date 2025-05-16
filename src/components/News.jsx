63abc30863c42d389e8c4d49a05a7e266828011e
Shubham <54595517+shubhamparashar@users.noreply.github.com>
2025-05-16 18:18:32 +0530

    updated news api

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -42,47 +42,39 @@ const News = ({ simplified }) => {
             </Select>
           </Col>
         )}
-        {cryptoNews.value.map((news, i) => (
+        {cryptoNews.articles?.map((news, i) => (
           <Col xs={24} sm={12} lg={8} key={i}>
             <Card hoverable className="news-card">
               <a href={news.url} target="_blank" rel="noreferrer">
                 <div className="news-image-container">
                   <Title className="news-title" level={4}>
-                    {news.name}
+                    {news.title}
                   </Title>
                   <img
                     style={{ maxHeight: "100px", maxWidth: "200px" }}
-                    src={news?.image?.thumbnail?.contentUrl || demoImage}
-                    alt={`Thumbnail for ${news.name}`}
+                    src={news.urlToImage || demoImage}
+                    alt={`Thumbnail for ${news.title}`}
                   />
-
                 </div>
                 <p>
-                  {news.description > 100
-                    ? `${news.description.substring(0, 100)}....`
+                  {news.description?.length > 100
+                    ? `${news.description.substring(0, 100)}...`
                     : news.description}
                 </p>
                 <div className="provider-container">
                   <div>
-                    <Avatar
-                      src={
-                        news.provider[0]?.image?.thumbnail?.contentUrl ||
-                        demoImage
-                      }
-                      alt="News-Provider"
-                    />
+                    <Avatar src={demoImage} alt="News-Provider" />
                     <Text className="provider-name">
-                      {news.provider[0].name}
+                      {news.source.name}
                     </Text>
                   </div>
-                  <Text>
-                    {moment(news.datePublished).startOf("ss").fromNow()}
-                  </Text>
+                  <Text>{moment(news.publishedAt).startOf("ss").fromNow()}</Text>
                 </div>
               </a>
             </Card>
           </Col>
         ))}
+
       </Row>
     </>
   );
--- src/services/crytoNewApi.js
+++ src/services/crytoNewApi.js
@@ -1,23 +1,20 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const cryptoNewsHeaders = {
-  "x-rapidapi-host": "bing-news-search1.p.rapidapi.com",
-  "x-rapidapi-key": "c58fda0ea8msh19cb2198150069bp114709jsnbbc359c54e98",
+const newsApiHeaders = {
+  'X-Api-Key': '120ea7f5290c4dc7926e9480cd9dc83b', 
 };
 
-const baseUrl = "https://bing-news-search1.p.rapidapi.com";
+const baseUrl = 'https://newsapi.org/v2';
 
-const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+const createRequest = (url) => ({ url, headers: newsApiHeaders });
 
 export const cryptoNewsApi = createApi({
-  reducerPath: "cryptoNewsApi",
+  reducerPath: 'cryptoNewsApi',
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     getCryptoNews: builder.query({
       query: ({ newsCategory, count }) =>
-        createRequest(
-          `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
-        ),
+        createRequest(`/everything?q=${newsCategory}&pageSize=${count}&sortBy=publishedAt`),
     }),
   }),
 });
